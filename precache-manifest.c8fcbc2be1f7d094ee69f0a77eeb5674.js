8d88c9d38d8b307d57d44a6b7c785db32bdb7da3
Mohammad Shishegar <mohammad127sh82@gmail.com>
2020-08-28 19:26:41 +0430

    Updates

---
+++ precache-manifest.c8fcbc2be1f7d094ee69f0a77eeb5674.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fb895f0d122c19893f201e47cfbba3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca33478e2bfa3306a9d8",
+    "url": "/static/css/main.850f770f.chunk.css"
+  },
+  {
+    "revision": "189d887be2cd90ce6629",
+    "url": "/static/js/2.05daa080.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.05daa080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca33478e2bfa3306a9d8",
+    "url": "/static/js/main.2816f3e6.chunk.js"
+  },
+  {
+    "revision": "edd8d49a81f0103c025f",
+    "url": "/static/js/runtime-main.fb0b04e8.js"
+  },
+  {
+    "revision": "f250e12628a7b2a12cb9141fa2e5cda1",
+    "url": "/static/media/back.f250e126.jpg"
+  },
+  {
+    "revision": "581fc6ad45a1428157168bfff7c40e03",
+    "url": "/static/media/gal-1.581fc6ad.jpeg"
+  },
+  {
+    "revision": "9865e700f1cd5e17135a6f75da6b1378",
+    "url": "/static/media/gal-10.9865e700.jpeg"
+  },
+  {
+    "revision": "5536a90f3836ebf8085ec0ae46984867",
+    "url": "/static/media/gal-11.5536a90f.jpeg"
+  },
+  {
+    "revision": "0709a3b0434b69b42cdd096e4d13f90f",
+    "url": "/static/media/gal-12.0709a3b0.jpeg"
+  },
+  {
+    "revision": "b4740c9c0d2cec5640904289867c8ce6",
+    "url": "/static/media/gal-13.b4740c9c.jpeg"
+  },
+  {
+    "revision": "486c8880b61fee96a95de85288a1e6ae",
+    "url": "/static/media/gal-14.486c8880.jpeg"
+  },
+  {
+    "revision": "ee7b3ca7ee80e0b5c62391c6ec93a671",
+    "url": "/static/media/gal-2.ee7b3ca7.jpeg"
+  },
+  {
+    "revision": "628f87aeedf507d437f6aeb12bcc4215",
+    "url": "/static/media/gal-3.628f87ae.jpeg"
+  },
+  {
+    "revision": "c6fff3a13ac92abd83a80c208c4dda53",
+    "url": "/static/media/gal-4.c6fff3a1.jpeg"
+  },
+  {
+    "revision": "4114ded407c6764a037d322452fd6ca0",
+    "url": "/static/media/gal-5.4114ded4.jpeg"
+  },
+  {
+    "revision": "aa2376b5a04a3bf548d9f1773f580da1",
+    "url": "/static/media/gal-6.aa2376b5.jpeg"
+  },
+  {
+    "revision": "6085a127abe2dab815ab22ec0a9135d1",
+    "url": "/static/media/gal-7.6085a127.jpeg"
+  },
+  {
+    "revision": "f30fbda69d5eca2f340eb6d4d093a914",
+    "url": "/static/media/gal-8.f30fbda6.jpeg"
+  },
+  {
+    "revision": "92758fee44f610a3b225c33ff90a9333",
+    "url": "/static/media/gal-9.92758fee.jpeg"
+  },
+  {
+    "revision": "d11b666fa6c694c620ff867c6669cf7f",
+    "url": "/static/media/hero.d11b666f.jpeg"
+  },
+  {
+    "revision": "5120ad222b9080b343187d3c51e851f3",
+    "url": "/static/media/house-1.5120ad22.jpeg"
+  },
+  {
+    "revision": "ed12ff11bf26663331ad01ff18d3ebfb",
+    "url": "/static/media/house-2.ed12ff11.jpeg"
+  },
+  {
+    "revision": "521c065d9e8bc6ff2ec850a3ea58a4bf",
+    "url": "/static/media/house-3.521c065d.jpeg"
+  },
+  {
+    "revision": "83ee1d1e0e83198aa12b8a63af3f8a2b",
+    "url": "/static/media/house-4.83ee1d1e.jpeg"
+  },
+  {
+    "revision": "721effb4a9ef7f3ac5e281cb13cda239",
+    "url": "/static/media/house-5.721effb4.jpeg"
+  },
+  {
+    "revision": "ee021f8ac6ebfadeab35a43e6ef6b306",
+    "url": "/static/media/house-6.ee021f8a.jpeg"
+  },
+  {
+    "revision": "ee16c7b8a84ac078afc2b46923cc6187",
+    "url": "/static/media/logo-bi.ee16c7b8.png"
+  },
+  {
+    "revision": "e5c174a02df208c513cfb932428e84c9",
+    "url": "/static/media/logo-forbes.e5c174a0.png"
+  },
+  {
+    "revision": "a473e3878ca044d42ba86f99b685ba47",
+    "url": "/static/media/logo-techcrunch.a473e387.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "764685dbee21c14ef7a112168e92b429",
+    "url": "/static/media/logo.764685db.png"
+  },
+  {
+    "revision": "406400ead91831b741f5246c50146142",
+    "url": "/static/media/realtor-1.406400ea.jpeg"
+  },
+  {
+    "revision": "c11e84dcdad4dbf1cb49ff5813ed6611",
+    "url": "/static/media/realtor-2.c11e84dc.jpeg"
+  },
+  {
+    "revision": "af9ff289ef4aa6e35a09b66ec1e42419",
+    "url": "/static/media/realtor-3.af9ff289.jpeg"
+  },
+  {
+    "revision": "24268b24588bddc750dc3b45bd820459",
+    "url": "/static/media/story-1.24268b24.jpeg"
+  },
+  {
+    "revision": "915f2bd1ebf80f9d775aa9954f294269",
+    "url": "/static/media/story-2.915f2bd1.jpeg"
+  }
+]);
